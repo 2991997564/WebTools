b86376a45f7b89e69542870a6c7e97b0883ac90c
2991997564 <tspyou@163.com>
2018-05-06 21:42:57 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+## 生活助手
+
+基于Vue.js 2.0开发的生活助手APP，将各类零散的日常工具APP，提供一个平台集中起来，为需要的人提供更加便捷快速的获取信息服务的渠道。
+
+## 安装部署
+
+1：下载
+
+```txt
+git clone https://github.com/helpcode/vue-web-tools.git
+cd vue-web-tools/
+```
+
+2：解决依赖并运行
+
+```txt
+//初始化
+npm install
+
+//dev 运行网站
+npm run dev
+
+//build 构建发布
+npm run build
+```
+
+
+感谢一些第三方网站提供的免费数据接口
---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+      <footer-component ref="footer"></footer-component>
+      <keep-alive>
+          <transition name="bounce" v-on:before-enter="beforeEnter">
+            <router-view ref="content"></router-view>
+          </transition>
+      </keep-alive>
+      <head-component ref="head"></head-component>
+  </div>
+  </div>
+</template>
+
+<script>
+import FooterComponent from './components/footer/footer'
+import HeadComponent from './components/head/head'
+export default {
+  name: 'app',
+  methods: {
+      beforeEnter(el){
+         // console.log(el)
+      }
+  },
+  components: {
+    FooterComponent,
+    HeadComponent
+  }
+}
+</script>
+
+<style scoped>
+    #content-component{
+        margin: 55px 0 50px 0;
+    }
+
+    .bounce-enter-active {
+        animation: bounce-in .5s;
+    }
+    .bounce-leave-active {
+        animation: bounce-out .5s;
+    }
+    @keyframes bounce-in {
+        0% {
+            transform: scale(0);
+        }
+        50% {
+            transform: scale(1.5);
+        }
+        100% {
+            transform: scale(1);
+        }
+    }
+    @keyframes bounce-out {
+        0% {
+            transform: scale(1);
+        }
+        50% {
+            transform: scale(1.5);
+        }
+        100% {
+            transform: scale(0);
+        }
+    }
+</style>
